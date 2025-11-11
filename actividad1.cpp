56c1a08c532831cf2db3a826994d9cd05a30b3c3
Valeria López Rodríguez <a01798944@tec.mx>
2025-11-10 19:35:05 -0600

    Programa Actividad 1

---
+++ actividad1.cpp
@@ -0,0 +1,18 @@
+//
+// Created by valer on 10/11/2025.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    float area, base, altura;
+
+    base = 5.0;
+    altura = 3.5;
+
+    area = base * altura;
+
+    cout << "El area del rectangulo es: " << area << endl;
+
+    return 0;
+}
